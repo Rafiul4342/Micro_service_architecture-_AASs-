2b770dc282e6f9c144478802e4b4514cfba48802
Rafiul Islam <rafiulislam4342@gmail.com>
2021-05-08 15:09:36 +0200

    this is the rabbit mq message bus and message

---
+++ RabbitMq_Ifat/RabbitMqBus.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using MassTransit.RabbitMqTransport;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMq_Ifat
+{
+    public class RabbitMqBus
+    {
+        public static IBusControl ConfigureBus(IServiceProvider provider, Action<IRabbitMqBusFactoryConfigurator, IRabbitMqHost>
+         registrationAction = null)
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri(BusConstants.RabbitMqUri), hst =>
+                {
+                    hst.Username(BusConstants.UserName);
+                    hst.Password(BusConstants.Password);
+                });
+
+                cfg.ConfigureEndpoints(provider);
+
+                registrationAction?.Invoke(cfg, host);
+            });
+        }
+    }
+}
